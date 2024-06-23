f399336b219acd392c25cd38293665857206ce02
shivrajlawhare <shivrajlawhare6@gmail.com>
2024-06-23 16:33:33 +0530

    styling remaining

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,73 @@
-import React, { useState } from 'react';
-import TransactionsTable from './components/TransactionsTable';
-import Statistics from './components/Statistics';
-import TransactionsBarChart from './components/BarChart';
-import TransactionsPieChart from './components/PieChart';
+import React, { useState } from "react";
+import TransactionsTable from "./components/TransactionsTable";
+import Statistics from "./components/Statistics";
+import TransactionsBarChart from "./components/BarChart";
+import TransactionsPieChart from "./components/PieChart";
 
 const App = () => {
-    const [month, setMonth] = useState('03');
-    const [search, setSearch] = useState('');
-    const [page, setPage] = useState(1);
+  const [month, setMonth] = useState("03");
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
 
-    return (
-        <div>
-            <select value={month} onChange={e => setMonth(e.target.value)}>
-                <option value="01">January</option>
-                <option value="02">February</option>
-                <option value="03">March</option>
-                <option value="04">April</option>
-                <option value="05">May</option>
-                <option value="06">June</option>
-                <option value="07">July</option>
-                <option value="08">August</option>
-                <option value="09">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-            </select>
-            <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder="Search transactions" />
-            <TransactionsTable month={month} search={search} page={page} setPage={setPage} />
-            <Statistics month={month} search={search} page={page} setPage={setPage} />
-            <TransactionsBarChart month={month} search={search} page={page} setPage={setPage} />
-            <TransactionsPieChart month={month} search={search} page={page} setPage={setPage} />
+  return (
+    <div className="bg-cyan-100 pt-4">
+      <div className="w-fit bg-white rounded-full px-5 py-12 mx-auto">
+        <p className="text-2xl font-bold text-gray-500">
+          Transaction <br /> Dashboard
+        </p>
+      </div>
+      <div className="flex justify-between items-center my-10 text-xl mx-56">
+        <div className="text-center">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search transactions"
+            className="text-center py-2 bg-yellow-200 rounded-full text-black"
+          />
         </div>
-    );
+        <div className="text-center">
+          <select
+            className="text-center py-2 bg-yellow-200 rounded-full text-black px-10"
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+          >
+            <option value="01">January</option>
+            <option value="02">February</option>
+            <option value="03">March</option>
+            <option value="04">April</option>
+            <option value="05">May</option>
+            <option value="06">June</option>
+            <option value="07">July</option>
+            <option value="08">August</option>
+            <option value="09">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+        </div>
+      </div>
+      <TransactionsTable
+        month={month}
+        search={search}
+        page={page}
+        setPage={setPage}
+      />
+      <Statistics month={month} search={search} page={page} setPage={setPage} />
+      <TransactionsBarChart
+        month={month}
+        search={search}
+        page={page}
+        setPage={setPage}
+      />
+      <TransactionsPieChart
+        month={month}
+        search={search}
+        page={page}
+        setPage={setPage}
+      />
+    </div>
+  );
 };
 
 export default App;
--- client/src/components/TransactionsTable.js
+++ client/src/components/TransactionsTable.js
@@ -1,61 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const TransactionsTable = ({ month = '03', search, page, setPage }) => {
-    const [transactions, setTransactions] = useState([]);
-    const [total, setTotal] = useState(0);
+const TransactionsTable = ({ month = "03", search, page, setPage }) => {
+  const [transactions, setTransactions] = useState([]);
+  const [total, setTotal] = useState(0);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await axios.get('https://roxiler-systems-challenge-server.onrender.com/api/transactions', {
-                params: { month, search, page, perPage: 10 }
-            });
-            setTransactions(response.data.transactions);
-            setTotal(response.data.total);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://roxiler-systems-challenge-server.onrender.com/api/transactions",
+        {
+          params: { month, search, page, perPage: 10 },
+        }
+      );
+      setTransactions(response.data.transactions);
+      setTotal(response.data.total);
+    };
 
-        fetchData();
-        console.log(total);
-    }, [month, search, page, total]);
+    fetchData();
+    console.log(total);
+  }, [month, search, page, total]);
 
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                        <th>Category</th>
-                        <th>Date of Sale</th>
-                        <th>Sold</th>
-                        <th>Image</th> 
-                    </tr>
-                </thead>
-                <tbody>
-                    {transactions.map(transaction => (
-                        <tr key={transaction._id}>
-                            <td>{transaction.title}</td>
-                            <td>{transaction.description}</td>
-                            <td>{transaction.price}</td>
-                            <td>{transaction.category}</td>
-                            <td>{new Date(transaction.dateOfSale).toLocaleDateString()}</td>
-                            <td>{transaction.sold ? 'Yes' : 'No'}</td>
-                            <td>
-                                {transaction.image ? (
-                                    <img src={transaction.image} alt="Transaction" style={{ width: '100px', height: 'auto' }} />
-                                ) : (
-                                    <span>No Image</span>
-                                )}
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <button disabled={page === 1} onClick={() => setPage(page - 1)}>Previous</button>
-            <button disabled={page * 10 >= total} onClick={() => setPage(page + 1)}>Next</button>
+  return (
+    <div className="mx-56">
+      <table class="table-auto bg-yellow-200  ">
+        <thead>
+          <tr>
+            <th className="border-2 border-black p-4 ">Title</th>
+            <th className="border-2 border-black p-4">Description</th>
+            <th className="border-2 border-black p-4">Price</th>
+            <th className="border-2 border-black p-4">Category</th>
+            <th className="border-2 border-black p-4">Date of Sale</th>
+            <th className="border-2 border-black p-4">Sold</th>
+            <th className="border-2 border-black p-4">Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction._id}>
+              <td className="border-2 border-black p-4">{transaction.title}</td>
+              <td className="border-2 border-black p-4">
+                {transaction.description}
+              </td>
+              <td className="border-2 border-black p-4">{transaction.price}</td>
+              <td className="border-2 border-black p-4">
+                {transaction.category}
+              </td>
+              <td className="border-2 border-black p-4">
+                {new Date(transaction.dateOfSale).toLocaleDateString()}
+              </td>
+              <td className="border-2 border-black p-4">
+                {transaction.sold ? "Yes" : "No"}
+              </td>
+              <td className="border-2 border-black p-4">
+                {transaction.image ? (
+                  <img
+                    src={transaction.image}
+                    alt="Transaction"
+                    style={{ width: "100px", height: "auto" }}
+                  />
+                ) : (
+                  <span>No Image</span>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="flex justify-between my-6">
+        <p>{`Page No: ${page}`}</p>
+        <div className="flex justify-between ">
+          <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+            Previous
+          </button>
+          <p className="mx-10">
+            -
+          </p>
+          <button
+            disabled={page * 10 >= total}
+            onClick={() => setPage(page + 1)}
+          >
+            Next
+          </button>
         </div>
-    );
+        <p>Per Page: 10</p>
+      </div>
+    </div>
+  );
 };
 
 export default TransactionsTable;
